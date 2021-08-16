686dc73240dd2a378b8e9d3d713180d3f67118e6
danycastyllo <d.castillo1@utp.edu.co>
2021-08-15 23:16:41 -0500

    Product list and add products

--- app/src/main/java/com/example/busment/ui/notifications/ProductsFragment.kt
+++ app/src/main/java/com/example/busment/ui/notifications/ProductsFragment.kt
@@ -1,14 +1,18 @@
 package com.example.busment.ui.notifications
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.busment.databinding.FragmentProductsBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.fragment_products.*
 
 class ProductsFragment : Fragment() {
 
@@ -19,21 +23,59 @@ class ProductsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         productsViewModel =
-            ViewModelProvider(this).get(ProductsViewModel::class.java)
+            ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(
+                ProductsViewModel::class.java
+            )
 
         _binding = FragmentProductsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        productsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        val lista: ListView = binding.lista
+        val button: FloatingActionButton = binding.floatingActionButton
+//        var listaProductos = listOf<Producto>()
+//        notificationsViewModel.listaProductos.observe(viewLifecycleOwner, Observer {
+//            listaProductos = it
+//        })
+//
+//        val adapter = ProductosAdapter(lista.context, productsViewModel.listaProductos)
+//
+//        lista.adapter = adapter
+
+
+        var listaProductos = emptyList<Producto>()
+
+        val database = AppDatabase.getDatabase(lista.context)
+
+        database.productos().getAll().observe(viewLifecycleOwner, Observer {
+            listaProductos = it!! // posiblemente la lista no se muestre por esto (recomendacion es volver a copiar el codigo y intentar resolverlo)
+
+            val adapter = ProductosAdapter(lista.context, listaProductos)
+
+            lista.adapter = adapter
         })
+
+        lista.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(lista.context, ProductoActivity::class.java)
+            intent.putExtra("id", listaProductos[position].idProducto)
+            startActivity(intent)
+        }
+
+        button.setOnClickListener {
+            val intent = Intent(lista.context, NuevoProductoActivity::class.java)
+            startActivity(intent)
+        }
+
         return root
     }
 
--- app/src/main/java/com/example/busment/ui/notifications/ProductsViewModel.kt
+++ app/src/main/java/com/example/busment/ui/notifications/ProductsViewModel.kt
@@ -3,11 +3,12 @@ package com.example.busment.ui.notifications
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.busment.R
 
 class ProductsViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
-    }
-    val text: LiveData<String> = _text
+    val producto = Producto("Camara", 100.0, "Camara ultimo modelo", R.drawable.ic_notifications_black_24dp)
+    val producto2 = Producto("PC", 1000.0, "16 GB RAM", R.drawable.ic_home_black_24dp)
+
+    val listaProductos = listOf(producto, producto2)
 }
